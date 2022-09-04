92ec38bee386bd159b3967ed5b6e4b75d7449932
Sreedhar K <55496113+sreegithub19@users.noreply.github.com>
2022-09-04 21:32:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -80,74 +80,6 @@ import { BrowserRouter as Router, Routes,Route, Link, renderMatches } from "reac
 3. Regular HTML page rendering through these <script> tags
 */}
 
-const codepen = `
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Code Editor</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-    body {
-        text-align: center;
-    }
-    
-    textarea {
-        width: 32%;
-        float: top;
-        min-height: 250px;
-        overflow: scroll;
-        margin: auto;
-        display: inline-block;
-        background: #F4F4F9;
-        outline: none;
-        font-family: Courier, sans-serif;
-        font-size: 14px;
-    }
-    
-    iframe {
-        bottom: 0;
-        position: relative;
-        width: 100%;
-        height: 35em;
-    }
-    </style>
-</head>
-<body>
-    <textarea id="html" placeholder="HTML"></textarea>
-    <textarea id="css" placeholder="CSS"></textarea>
-    <textarea id="js" placeholder="JavaScript"></textarea>
-    <iframe id="code"></iframe>
-    <script type="text/javascript">
-                        function compile() {
-        var html = document.getElementById("html");
-        var css = document.getElementById("css");
-        var js = document.getElementById("js");
-        var code = document.getElementById("code").contentWindow.document;
-        
-        document.body.onkeyup = function(){
-            code.open();
-            code.writeln(html.value+"<style>"+css.value+"</style>"+"<script>" + js.value + "<\/script>");
-            code.close();
-        };
-        };
-    
-    compile();  
-    </script>
-</body>
-</html>
-`;
-
- function Codepen() { 
-  //const inlineScript = '(() => { alert("Hello World!") })();'
-  return (
-    <div>
-    <h2 onClick={() =>
-    document.write(codepen)}>codepen</h2>
-    </div>
-  )
-  } 
-  
-
   const hangman = `
    <!DOCTYPE html>
 <html lang="en">
@@ -6905,9 +6837,6 @@ html[subframe] body {
               <li> 
                 <Link to="/dino/">Dino</Link> 
               </li> 
-	      <li> 
-                <Link to="/codepen/">Codepen</Link> 
-              </li> 
             </ul> 
           </nav> 
           <Routes>
@@ -6921,7 +6850,6 @@ html[subframe] body {
           <Route path="/solitaire/" element={<Solitaire />} />
           <Route path="/tilt_maze/" element={<Tilted_maze />} />
           <Route path="/dino/" element={<Dino />} />
-	  <Route path="/codepen/" element={<Codepen />} />
           </Routes>
         </div> 
       </Router> 
