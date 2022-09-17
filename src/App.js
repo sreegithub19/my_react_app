ceed8ab9173e24cae7c118bd942f2699c95f9b1a
K <sreedhar.k@rakuten.com>
2022-09-17 11:28:34 +0530

    Added changes

--- src/App.js
+++ src/App.js
@@ -6868,6 +6868,57 @@ function Codepen(){
   )
   }
 
+const sass_ = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sass.js/0.9.12/sass.sync.min.js"></script>
+        <style type="scss">
+@mixin l2d($color,$strength){
+    background:linear-gradient(lighten($color,$strength),darken($color,$strength));
+    &:active{
+        background:linear-gradient(darken($color,$strength),lighten($color,$strength));
+    }
+}
+body{
+    @include l2d(#aa2424,10%);
+    min-height:100vh;
+}
+#hello{
+    margin:auto auto auto auto;
+    width:96vw;
+    user-select:none;
+    &:after{
+        content:" 😉";
+    }
+}
+        </style>
+        
+        <script>
+            Sass.compile(document.querySelector("style[type=scss]").innerHTML,function(res){
+    var s=document.createElement("style");
+    s.innerHTML=res.text;
+    res.formatted&&console.error(res.formatted); //if error console.error it.
+    res.text&&document.head.appendChild(s);// append style element only if no error.
+});
+        </script>
+    </head>
+    <body>
+        <h1 id="hello">Hello, this is styled using SCSS.</h1>
+    </body>
+</html>
+`
+
+  function Sass(){
+   return (
+    <div>
+    <h2 onClick={() =>
+    document.write(sass_)}>Sass_</h2>
+    </div>
+  )
+  }
+
 
   function App(){
 
@@ -6881,6 +6932,9 @@ function Codepen(){
               </li> 
               <li> 
                 <Link to="/hangman/">hangman</Link> 
+              </li> 
+              <li> 
+                <Link to="/sass_/">sass</Link> 
               </li> 
               <li> 
                 <Link to="/clock/">clock</Link> 
@@ -6920,6 +6974,7 @@ function Codepen(){
           <Route path="/tilt_maze/" element={<Tilted_maze />} />
           <Route path="/dino/" element={<Dino />} />
           <Route path="/codepen/" element={<Codepen />} />
+          <Route path="/sass_/" element={<Sass />} />
           </Routes>
         </div> 
       </Router>  
